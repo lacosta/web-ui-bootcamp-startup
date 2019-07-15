64a1d55a5a5de3c1c593881cc6346214fd7f3533
lacosta <leandroacosta.contact@gmail.com>
2019-07-15 20:08:15 -0300

    Added interface for the palindrome function

--- Topic-3-Javascript/palindrome-001.js
+++ Topic-3-Javascript/palindrome-001.js
@@ -1,10 +1,12 @@
-function checkPalindrome(word){
-  if(typeof word === 'number'){
-    console.log(`The type: ${typeof word} isn\'t valid`);
+let button = document.querySelector('button');
+button.onclick = function checkPalindrome(){
+  word = document.getElementById('userWrite').value;
+  var userWrite = parseInt(word);
+  if(!Number.isNaN(userWrite)){
+    alert(`The type: ${typeof word} isn\'t valid`);
   } else {
     word = word.toLowerCase();
     var isPalindrome = word.split('').reverse().join('');
-    word == isPalindrome ? console.log('This is a palindrome') : console.log('This isn\'t a palindrome');
+    word == isPalindrome ? alert('This is a palindrome') : alert('This isn\'t a palindrome');
   }
 }
-checkPalindrome('RaceCar');
