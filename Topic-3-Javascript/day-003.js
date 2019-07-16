9612385a1da695d1efbbc58795af8af881b5dea0
lacosta <leandroacosta.contact@gmail.com>
2019-07-15 22:39:40 -0300

    Added days function, css modified and enhancemets to previous functions

--- Topic-3-Javascript/day-003.js
+++ Topic-3-Javascript/day-003.js
@@ -1,11 +1,14 @@
-const week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-function getDayAfter(day,n){
-  if(typeof n !== 'number'){
-    console.log(`The ${typeof n} isn\'t valid`);
+let button = document.querySelector('button');
+const week = 
+['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+button.onclick = function getDayAfter(){
+  day = document.getElementById('userWriteDay').value;
+  n = document.getElementById('userWriteN').value;
+  if(typeof n === 'number'){
+    document.getElementById('salida').innerText = `The ${typeof n} isn\'t valid`;
   } else {
     var position = week.indexOf(day);
     var resp = (n % 7) + position;
-    return console.log(week[resp]);
+    return document.getElementById('salida').innerText = (week[resp]);
   }
-}
-getDayAfter('Monday',4);
+} 
--- Topic-3-Javascript/palindrome-002.js
+++ Topic-3-Javascript/palindrome-002.js
@@ -1,11 +1,13 @@
-function checkPalindrome2(word2){
-  if(typeof word2 === 'number'){
-    console.log(`The type: ${typeof word2} isn\'t valid`);
+let button = document.querySelector('button');
+button.onclick = function checkPalindrome2(){
+  word2 = document.getElementById('userWrite').value;
+  var userWrite = parseInt(word2);
+  if(!Number.isNaN(userWrite)){
+    document.getElementById('salida').innerText = `The type: ${typeof userWrite} isn\'t valid`;
   } else {
     word2 = word2.replace(/\W/g, '');
     word2 = word2.toLowerCase();
-    var isPalindrome2 = word2.split('').reverse().join('');
-    word2 == isPalindrome2 ? console.log('This is a palindrome') : console.log('This isn\'t a palindrome');
+    var reversedWord = word2.split('').reverse().join('');
+    document.getElementById('salida').innerText = word2 == reversedWord ? 'This is a palindrome' : 'This isn\'t a palindrome';
   }
 }
-checkPalindrome2('Eva, can I see bees in a cave?');
