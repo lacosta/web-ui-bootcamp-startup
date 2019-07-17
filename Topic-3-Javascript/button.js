4b4cc94b130eaf10b604d731f8aea30c919afd82
lacosta <leandroacosta.contact@gmail.com>
2019-07-16 22:53:10 -0300

    Created chuck norris joke function

--- Topic-3-Javascript/button.js
+++ Topic-3-Javascript/button.js
@@ -11,3 +11,17 @@ button.onclick = function fadein(){
   let hidden = document.querySelector('section');
   hidden.classList.remove('hidden');
 }
+
+const button3 = document.getElementById('functionChuck');
+button3.addEventListener('click', () => {
+  let xhr 
+  if(window.XMLHttpRequest) xhr = new XMLHttpRequest()
+  else xhr = new ActiveXObject('Microsoft.XMLHTTP')
+  xhr.open('GET', 'http://api.icndb.com/jokes/random')
+  xhr.addEventListener('load', (data) => {
+    const apiInfo = JSON.parse(data.target.response)
+    chuckP = apiInfo.textContent = `${apiInfo.value.joke}`
+    document.getElementById('chuckJoke').innerHTML = (chuckP)
+  })
+  xhr.send()
+})
