3b5d2405fc70c7db798aff52620f30e819e3660d
lacosta <49215594+lacosta@users.noreply.github.com>
2019-07-20 21:47:03 -0300

    Fix minor things, like ";" and ternary operator syntax
    Fix minor things, like ";" and ternary operator syntax.

--- Topic-3-Javascript/button.js
+++ Topic-3-Javascript/button.js
@@ -14,43 +14,45 @@ button.onclick = function fadein(){
 
 const button3 = document.getElementById('functionChuck');
 button3.addEventListener('click', () => {
-  let xhr 
-  if(window.XMLHttpRequest) xhr = new XMLHttpRequest()
-  else xhr = new ActiveXObject('Microsoft.XMLHTTP')
-  xhr.open('GET', 'http://api.icndb.com/jokes/random')
+  let xhr;
+  if (window.XMLHttpRequest) {
+  xhr = new XMLHttpRequest();
+  }
+  else xhr = new ActiveXObject('Microsoft.XMLHTTP');
+  xhr.open('GET', 'http://api.icndb.com/jokes/random');
   xhr.addEventListener('load', (data) => {
-    const apiInfo = JSON.parse(data.target.response)
-    chuckP = apiInfo.textContent = `${apiInfo.value.joke}`
-    document.getElementById('chuckJoke').innerHTML = (chuckP)
+    const apiInfo = JSON.parse(data.target.response);
+    chuckP = apiInfo.textContent = `${apiInfo.value.joke}`;
+    document.getElementById('chuckJoke').innerHTML = (chuckP);
   })
-  xhr.send()
+  xhr.send();
 }) 
 
-const button4 = document.getElementById('functionChuck2')
-const parrafo = document.getElementById('chuckJoke2')
+const button4 = document.getElementById('functionChuck2');
+const parrafo = document.getElementById('chuckJoke2');
 button4.addEventListener('click', () => {
   fetchData('http://api.icndb.com/jokes/random').then(data => {
-    chuckJoke2.innerHTML = data.value.joke
+    chuckJoke2.innerHTML = data.value.joke;
   }).catch(err => {
     document.getElementById('idSection').style.backgroundColor = "red";
   })
 })
 
 function fetchData(url) {
-  return fetch(url).then(x => x.json())
+  return fetch(url).then(x => x.json());
 }
 
-const button5 = document.getElementById('listButton')
+const button5 = document.getElementById('listButton');
 button5.addEventListener('click', () => {
   fetch('https://api.github.com/search/repositories?q=JavaScript')
     .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
     .then(res => res.json())
     .then(res => {
-      const list = document.getElementById('list')
+      const list = document.getElementById('list');
       for (const repoInfo of res.items){
-        const listItem = document.createElement('LI')
-        listItem.textContent = `${repoInfo.full_name}`
-        list.appendChild(listItem)
+        const listItem = document.createElement('LI');
+        listItem.textContent = `${repoInfo.full_name}`;
+        list.appendChild(listItem);
       }
     })
 })
@@ -61,6 +63,8 @@ const searchBook = document.getElementById('searchForm');
     const repos = list.getElementsByTagName('li');
     Array.from(repos).forEach(function(repo){
       const title = repo.textContent;
-      title.toLowerCase().indexOf(term) != -1 ? repo.style.display = 'block' : repo.style.display = 'none'
+      title.toLowerCase().indexOf(term) != -1 
+      ? repo.style.display = 'block'
+      : repo.style.display = 'none'
   })
 })
